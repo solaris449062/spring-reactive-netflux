0fb7eec32ca0703189f092116e02ca599864b4b7
solaris449062 <spark@spark.local>
2023-03-05 12:36:37 -0700

    initial commit

---
+++ src/main/java/com/springreactive/netflux/NetfluxApplication.java
@@ -0,0 +1,13 @@
+package com.springreactive.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springreactive/netflux/NetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreactive.netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
